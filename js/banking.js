9f380daa3fc037c3f7497451e449d6270374fee3
AHMED NABIB <nabibpallab@gmail.com>
2022-05-13 18:08:42 +0600

    deposit complete

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,37 @@
+
+
+
 document.getElementById('deposit-button').addEventListener('click', function () {
-    console.log('hello word')
+
+    let deposittotal = 0;
+    const depositInput = document.getElementById('deposit-input');
+
+    const newdepositAmountText = depositInput.value;
+    const newdepositAmount =parseFloat(newdepositAmountText)
+   //console.log(newdepositAmount)
+   
+    
+    const depositTotal = document.getElementById('deposit-total');
+
+    const previousDepositText = depositTotal.innerText;
+
+   
+
+    const previousDepositAmount = parseFloat(previousDepositText);
+    //  console.log(previousDepositAmount);
+
+    const newDepositTotal = newdepositAmount + previousDepositAmount
+    
+    // console.log(newDepositTotal)
+
+
+    depositTotal.innerText = newDepositTotal
+ 
+    
+
+  
+
+    depositInput.value =''
+
+
 })
